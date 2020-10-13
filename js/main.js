eaadf98ba664155c1a2783d18405291972a25afb
guilherme-biancardi <61367626+guilherme-biancardi@users.noreply.github.com>
2020-10-13 12:39:17 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -6,11 +6,15 @@ const imei = _.$('.imei'),
     error = _.$('.error'),
     buttons = [_.$('.btn'), _.$('.btnRes')]
 
-
+imei.oninput = () => {
+    imei.value = imei.value.replace(/[^0-9]/g, '');
+}
 _.click(buttons[0], () => {
     let text = imei.value
 
     if (text.length < 15) {
+        _.$('.content').style.animation = 'error .1s';
+        _.$('.content').style.animation = 'animation-iteration-count: 3';
         _.show(error)
     } else {
         _.hide(error)
